dc14c52af7105376d913efc48bb6037f8364702d
Davesoon <daveson@tuta.io>
2021-02-21 21:09:47 +0100

    phone validation

--- form.php
+++ form.php
@@ -6,17 +6,18 @@
         $everything_OK=true;
         //Sprawdź poprawność imienia
         $firstname = $_POST['firstname'];
-        //Sprawdzenie długości imienia
         if((strlen($firstname)<3) || (strlen($firstname)>20))
         {
             $everything_OK=false;
             $_SESSION['e_firstname']="Imię musi posiadać od 3 do 20 znaków!";
         }
-        // if(ctype_alnum($email)==false)
-        // {
-        //     $everything_OK=false;
-        //     $_SESSION['e_email']="Adres e-mail może składać się tylko z liter i cyfr (bez polskich znaków)";
-        // }
+        //Sprawdź poprawność nazwiska
+        $lastname = $_POST['lastname'];
+        if((strlen($lastname)<3) || (strlen($lastname)>30))
+        {
+            $everything_OK=false;
+            $_SESSION['e_lastname']="Nazwisko musi posiadać od 3 do 30 znaków!";
+        }
 
         // Sprawdź poprawność adresu email
         $email = $_POST['email'];
@@ -25,6 +26,19 @@
         {
             $everything_OK=false;
             $_SESSION['e_email']="Podaj poprawny adres e-mail!";
+        }
+        // if(ctype_alnum($email)==false)
+        // {
+        //     $everything_OK=false;
+        //     $_SESSION['e_email']="Adres e-mail może składać się tylko z liter i cyfr (bez polskich znaków)";
+        // }
+
+        // Sprawdź poprawność telefonu
+        $phone = $_POST['phone'];
+        if(ctype_digit($phone)==false)
+        {
+            $everything_OK=false;
+            $_SESSION['e_phone']="Numer telefonu może składać się tylko z cyfr (bez kierunkowego, myślników i spacji)";
         }
 
         if($everything_OK==true)
@@ -65,6 +79,14 @@
         ?>
 
         Nazwisko: <input type="text" name="lastname"><br>
+        <?php
+            if(isset($_SESSION['e_lastname']))
+            {
+                echo '<div class="error">'.$_SESSION['e_lastname'].'</div>';
+                unset($_SESSION['e_lastname']);
+            }
+        ?>
+
         E-mail: <input type="text" name="email"><br>
         <?php
             if(isset($_SESSION['e_email']))
@@ -75,6 +97,14 @@
         ?>
 
         Telefon: <input type="text" name="phone"><br>
+        <?php
+            if(isset($_SESSION['e_phone']))
+            {
+                echo '<div class="error">'.$_SESSION['e_phone'].'</div>';
+                unset($_SESSION['e_phone']);
+            }
+        ?>
+
         Dodatkowe informacje: <input type="text" name="info"><br>
         <label>
             <input type="checkbox" name="regulations"> Akceptuję regulamin
