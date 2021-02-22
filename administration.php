3d404a9b6302bfb25ba12f19064f68dd9ed4a011
Davesoon <daveson@tuta.io>
2021-02-22 16:30:00 +0100

    extract db records

--- administration.php
+++ administration.php
@@ -19,5 +19,36 @@
 <?php
     echo "<p>Witaj ".$_SESSION['login'].'!</p>';
     echo "<a href='logout.php'>Wyloguj się!</a>";
+
+    require_once "connect.php";
+    mysqli_report(MYSQLI_REPORT_STRICT);
+
+    try
+    {
+        $connection = new mysqli($host, $db_user, $db_password, $db_name);
+        if($connection->connect_errno!=0)
+        {
+            throw new Exception(mysqli_connect_errno());
+        }
+        else
+        {
+            $query = "SELECT * FROM members";
+            $result = $connection->query($query);
+            while($row = $result->fetch_assoc()) 
+                {
+                    echo "<br>";
+                    foreach($row as $value) 
+                    {
+                        echo $value;
+                    }
+                }
+        }
+    }
+    catch(Exception $e)
+    {
+        echo '<span style="color:red;">Błąd serwera! Przepraszamy za niedogodności i prosimy o rejestrację w innym terminie!</span>';
+        echo '<br>Informacja developerska: '.$e;
+    }
+    
 ?>
 </body>
