663ab4d318d2ed86aace5b215126622000381aa9
Davesoon <daveson@tuta.io>
2021-03-13 22:43:36 +0100

    basic logo & header

--- new-member.php
+++ new-member.php
@@ -5,29 +5,34 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>FORMULARZ</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dołącz do nas!</title>
     <link rel="stylesheet" href="style.css">
     <script src="https://www.google.com/recaptcha/api.js"></script>
 </head>
 <body>
-    <header>
-        <img src="images/dolacz.png" alt="">
-    </header>
-    <form method="post" enctype="multipart/form-data">
+    <div >
+        <header>
+            <img src="img/logo.png">
+            <h1>Dołącz do nas!</h1>
+        </header>
+        <main>
+            <form method="post" enctype="multipart/form-data">
 
-        <input type="hidden" value="<?php echo date('Y-m-d'); ?>" name="date">
+                <input type="hidden" value="<?php echo date('Y-m-d'); ?>" name="date">
 
-        <?php include "form/name-mail-phone.php"; ?>
+                <?php include "form/name-mail-phone.php"; ?>
 
-        <?php include "form/provinces.php"; ?>
+                <?php include "form/provinces.php"; ?>
 
-        <?php include "form/community-info-file-regulations-captcha.php"; ?>
+                <?php include "form/community-info-file-regulations-captcha.php"; ?>
 
-        <input type="submit" value="Wyślij formularz" name="submit">
+                <input type="submit" value="Wyślij formularz" name="submit">
 
-    </form>
+            </form>
+        </main>
+    </div>
     <script>
         function onSubmit(token) {document.getElementById("demo-form").submit();}
     </script>
-
 </body>
